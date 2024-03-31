8eb635b1d7c72af2a7d93e14e1bedb13dc87f991
JvsonKee <jasonk2723@gmail.com>
2024-03-31 11:47:04 -0600

    started navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+const NavBar = () => {
+    return(
+        <div>
+            <div>DIME</div>
+            <div>Transactions</div>
+            <div>Budgets & Goals</div>
+            <div>Savings</div>
+            <div>Account</div>
+        </div>
+    )
+}
+
+export default NavBar;
