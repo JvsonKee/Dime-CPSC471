fbb6d339d1fd5640b092076ba38f9f82ad5dc96f
JvsonKee <jasonk2723@gmail.com>
2024-04-01 13:24:58 -0600

    new colours

--- client/src/styles/Global.js
+++ client/src/styles/Global.js
@@ -7,6 +7,12 @@ const GlobalStyle = createGlobalStyle`
         // black/blue theme
         --dime-green: #1ED8AB;
         --dime-dark-black: #303030;
+        --dark-grey: #9BA9B7;
+        --light-grey: #ECEFFF;
+    }
+
+    body {
+        background-color: var(--light-grey);
     }
 
     body, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a {
