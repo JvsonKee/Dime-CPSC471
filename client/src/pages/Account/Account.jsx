c1be4e2f2a822c9bca15e589943fe53de797a39e
JvsonKee <jasonk2723@gmail.com>
2024-04-01 13:24:10 -0600

    navbar and main content spacing

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useNavigate } from "react-router-dom"
 import {useLocation} from 'react-router-dom';
 import NavBar from '../../components/NavBar';
+import { PageContainer, MainContainer } from '../../styles/Containers';
 
 
 const Account = () => {
@@ -38,30 +39,32 @@ const Account = () => {
     }
 
     return (
-        <div>
-            <NavBar />
-            <div>Welcome, {user.fName}</div>
-            <button>
-                <Link to="/home" state= {{account: user}}>Return to homepage</Link>
-            </button>
-            <button>
-                <Link to="/updateemail" state= {{account: user}}>Update my email</Link>
-            </button>
-            <button>
-                <Link to="/changepassword" state= {{account: user}}>Change my password</Link>
-            </button>
-            <button>
-                <Link to="/income" state= {{account: user}}>View income</Link>
-            </button>
-            <button>
-                <Link to="/paymentmethods" state= {{account: user}}>View payment methods</Link>
-            </button>
-            <button onClick = {()=>handleDelete()}>Delete my account</button>
-            {
-                user.premium === "y" ? <button onClick={premiumToStandard}>Become a Standard User</button> : 
-                user.premium === "n" ? <button onClick={standardToPremium}>Become a Premium User</button> : null
-            }
-        </div>
+        <PageContainer>
+            <NavBar account={user}/>
+            <MainContainer>
+                <div>Welcome, {user.fName}</div>
+                <button>
+                    <Link to="/home" state= {{account: user}}>Return to homepage</Link>
+                </button>
+                <button>
+                    <Link to="/updateemail" state= {{account: user}}>Update my email</Link>
+                </button>
+                <button>
+                    <Link to="/changepassword" state= {{account: user}}>Change my password</Link>
+                </button>
+                <button>
+                    <Link to="/income" state= {{account: user}}>View income</Link>
+                </button>
+                <button>
+                    <Link to="/paymentmethods" state= {{account: user}}>View payment methods</Link>
+                </button>
+                <button onClick = {()=>handleDelete()}>Delete my account</button>
+                {
+                    user.premium === "y" ? <button onClick={premiumToStandard}>Become a Standard User</button> : 
+                    user.premium === "n" ? <button onClick={standardToPremium}>Become a Premium User</button> : null
+                }
+            </MainContainer>
+        </PageContainer>
     )
 }
 
