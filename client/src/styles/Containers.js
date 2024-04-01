a061d6d852d9b4d52b75b46e65a91f22bd6eb78d
JvsonKee <jasonk2723@gmail.com>
2024-04-01 13:25:16 -0600

    main content container added

--- client/src/styles/Containers.js
+++ client/src/styles/Containers.js
@@ -2,4 +2,10 @@ import styled from "styled-components"
 
 export const PageContainer = styled.div`
     display: flex;
+    gap: 10px;
+`
+
+export const MainContainer = styled.div`
+    width: 80%;
+    background-color: pink;
 `
