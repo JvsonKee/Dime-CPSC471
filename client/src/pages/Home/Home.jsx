1b62cffd80d28142c3473ecdcd36c1958c7a2a7d
JvsonKee <jasonk2723@gmail.com>
2024-04-01 13:24:29 -0600

    navbar and content spacing

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import {useLocation} from 'react-router-dom';
 import NavBar from '../../components/NavBar';
-
+import { MainContainer, PageContainer } from '../../styles/Containers';
 
 const Home = () => {
 
@@ -10,34 +10,36 @@ const Home = () => {
     let user = location.state.account;
 
     return (
-    <div>
-        <NavBar />
-        <button>
-            <Link to ="/">Log off</Link>
-        </button>
-        <button>
-            <Link to="/transactions" state= {{userID: user.userID, premium: user.premium}}>Transactions</Link>
-        </button>
-        <button>
-        <Link to="/budgets" state= {{userID: user.userID, premium: user.premium}}>Budgets</Link>
-        </button>
-        <button>
-        <Link to="/savings" state= {{userID: user.userID, premium: user.premium}}>Savings</Link>
-        </button>
-        <button>
-        <Link to="/goals" state= {{userID: user.userID, premium: user.premium}}>Goals</Link>
-        </button>
-        <button>
-        <Link to="/account" state= {{account: location.state.account}}>Account</Link>
-        </button>
-        {
-            user.premium === "y" ? 
+    <PageContainer>
+        <NavBar account={user}/>
+        <MainContainer>
             <button>
-                <Link to="/dashboard" state= {{userID: user.userID, premium: user.premium}}>Dashboard</Link>
-            </button> : 
-            null
-        }
-    </div>
+                <Link to ="/">Log off</Link>
+            </button>
+            <button>
+                <Link to="/transactions" state= {{userID: user.userID, premium: user.premium}}>Transactions</Link>
+            </button>
+            <button>
+            <Link to="/budgets" state= {{userID: user.userID, premium: user.premium}}>Budgets</Link>
+            </button>
+            <button>
+            <Link to="/savings" state= {{userID: user.userID, premium: user.premium}}>Savings</Link>
+            </button>
+            <button>
+            <Link to="/goals" state= {{userID: user.userID, premium: user.premium}}>Goals</Link>
+            </button>
+            <button>
+            <Link to="/account" state= {{account: location.state.account}}>Account</Link>
+            </button>
+            {
+                user.premium === "y" ? 
+                <button>
+                    <Link to="/dashboard" state= {{userID: user.userID, premium: user.premium}}>Dashboard</Link>
+                </button> : 
+                null
+            }
+        </MainContainer>
+    </PageContainer>
     )
 }
 
