dbee67b0e71d62d7d23e1a1481ca0e18db74ace5
JvsonKee <jasonk2723@gmail.com>
2024-04-03 00:21:45 -0600

    Home page structure

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,24 +2,53 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import {useLocation} from 'react-router-dom';
 import NavBar from '../../components/NavBar';
-import { ContentContainer, MainContainer, PageContainer, Content } from '../../styles/Containers';
+import { ContentContainer, MainContainer, PageContainer, Content, CardContainer } from '../../styles/Containers';
+import { useNavigate } from 'react-router-dom';
+import { Matrix, RectangleBox, SquareBox, TopBottom, VerticalBox, Top, Bottom } from './Home.styled';
 
 const Home = () => {
 
+    const navigate = useNavigate();
     const location = useLocation();
     let user = location.state.account;
+    
+    const sendTo = (path) => {
+        navigate(path, {state: {account: user}})
+    }
 
     return (
     <PageContainer>
         <NavBar account={user}/>
         <MainContainer>
             <ContentContainer>
-                <Content>
-                    <h1>Welcome, {user.fName.charAt(0).toUpperCase() + user.fName.slice(1)}</h1>
-                    <button>
-                        <Link to ="/">Log off</Link>
-                    </button>
-                    <button>
+                <CardContainer>
+                    <Top>
+                        <SquareBox onClick={() => sendTo('/savings')}>
+                            Balance (dont click on this)
+                        </SquareBox>
+                        <SquareBox onClick={() => sendTo('/income')}>
+                            Incoming
+                        </SquareBox>
+                        <SquareBox>
+                            Calendar
+                        </SquareBox>
+                    </Top>
+                    <Bottom>
+                        <Matrix>
+                            <TopBottom>
+                                <RectangleBox>
+                                    Budgets & Goals
+                                </RectangleBox>
+                                <RectangleBox>
+                                    Premium Feature
+                                </RectangleBox>
+                            </TopBottom>
+                            <VerticalBox onClick={() => sendTo('/transactions')}>
+                                Recent Transactions
+                            </VerticalBox>
+                        </Matrix>
+                    </Bottom>
+                    {/* <button>
                         <Link to="/transactions" state= {{account: location.state.account}}>Transactions</Link>
                     </button>
                     <button>
@@ -41,7 +70,10 @@ const Home = () => {
                         </button> : 
                         null
                     }
-                </Content>
+                    <button>
+                        <Link to ="/">Log off</Link>
+                    </button> */}
+                </CardContainer>
             </ContentContainer>
         </MainContainer>
     </PageContainer>
--- client/src/pages/Home/Home.styled.js
+++ client/src/pages/Home/Home.styled.js
@@ -6,3 +6,53 @@ export const HomeContent = styled.div`
     height: 95%;
     width: 95%;
 `
+
+export const CardBase = styled.div`
+    display: flex;
+    background-color: white;
+    border-radius: 20px;
+    padding: 2%;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+export const SquareBox = styled(CardBase)`
+    width: 28.1%;
+`
+
+export const RectangleBox = styled(CardBase)`
+    height: 44%;
+`
+
+export const VerticalBox = styled(CardBase)`
+    width: 28%;
+`
+
+export const Matrix = styled.div`
+    display: flex;
+    gap: 2%;
+    width: 100%;
+    height: 100%;
+`
+
+export const TopBottom = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4%;
+    width: 66%;
+`
+
+export const Top = styled.div`
+    display: flex;
+    gap: 2%;
+    width: 100%;
+    height: 35%;
+`
+
+export const Bottom = styled.div`
+    display: flex;
+    width: 100%;
+    height: 65%;
+`
