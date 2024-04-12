66caaabfa7c1794dbb99f7918135a36947e3ee69
hannah-ku <93306711+hannah-ku@users.noreply.github.com>
2024-04-12 16:14:30 -0600

    transactionsupdated

--- client/src/pages/Transactions/Transactions.jsx
+++ client/src/pages/Transactions/Transactions.jsx
@@ -1,6 +1,4 @@
 import React, { useContext } from 'react'
-import {useState} from 'react'
-import {useEffect} from 'react'
 import axios from 'axios'
 import { Link } from "react-router-dom"
 import {useLocation} from 'react-router-dom';
@@ -10,55 +8,53 @@ import { TransactionsContainer, TransactionItem, TransactionButton, ButtonContai
 
 const Transactions = () => {
     const [user, setUser] = useContext(UserContext)
-    const [transactions, setTransactions] = useState([])
+  
 
     const navigate = useNavigate()
     const location = useLocation();
 
+    let transactions = location.state.transactions
     const handleDelete = async(transaction) => {
         try{
             await axios.delete("http://localhost:8800/deletetransaction/" + transaction);
-            navigate("/transactions")
+            let j = 0
+            for (let i = 0; i < location.state.transactions.length;  i++) {
+                if (location.state.transactions[i].transactionID === parseInt(transaction)) {
+                    j = i
+                    break
+                }
+            }
+            console.log(j)
+            location.state.transactions.splice(j,1)
+            navigate("/transactions", {state: {account: user, transactions: location.state.transactions}})
         }catch(err) {
             console.log(err)
         }
     }
 
     const handleReceipt = async(transaction) =>{
-        navigate("/receipts", {state: {account:user, transactionID: transaction}})
+        navigate("/receipts", {state: {account:user, transactions: location.state.transactions, transactionID: transaction}})
     }
     
-    useEffect(() => {
-        const fetchAllTransaction = async () => {
-            try{
-                const res = await axios.get("http://localhost:8800/transactions/" + user.userID)
-                setTransactions(res.data)
-                console.log(res)
-            }catch(err){
-                console.log(err)
-            }
-        }
-        fetchAllTransaction()
-    },[user.userID])
     return (
         <TransactionsContainer>
             <Title>Transactions</Title>
-            {transactions.map((transaction) => (
-                <TransactionItem key={transaction.transactionID}>
-                    <h2>Title: {transaction.title}</h2>
-                    <h2>Payment Method: {transaction.payment_method}</h2>
-                    <h2>Amount: ${transaction.amount}</h2>
-                    <h2>Date: {transaction.tDay} / {transaction.tMonth} / {transaction.tYear}</h2>
+            {transactions.map((transactions) => (
+                <TransactionItem key={transactions.transactionID}>
+                    <h2>Title: {transactions.title}</h2>
+                    <h2>Payment Method: {transactions.payment_name}</h2>
+                    <h2>Amount: ${transactions.amount}</h2>
+                    <h2>Date: {transactions.tDay} / {transactions.tMonth} / {transactions.tYear}</h2>
                     <TransactionButton>
-                        <Link to="/updatetransaction" state={{ transactionID: transaction.transactionID }}>Update</Link>
+                        <Link to="/updatetransaction" state= {{account: user, transactions: location.state.transactions, transactionID: transactions.transactionID}}>Update</Link>
                     </TransactionButton>
-                    <TransactionButton onClick={() => handleDelete(transaction.transactionID)}>Delete</TransactionButton>
-                    <TransactionButton onClick={() => handleReceipt(transaction.transactionID)}>View receipts</TransactionButton>
+                    <TransactionButton onClick={() => handleDelete(transactions.transactionID)}>Delete</TransactionButton>
+                    <TransactionButton onClick={() => handleReceipt(transactions.transactionID)}>View receipts</TransactionButton>
                 </TransactionItem>
             ))}
             <ButtonContainer>
                 <TransactionButton>
-                    <Link to="/newtransaction">New Transaction</Link>
+                    <Link to="/newtransaction" state= {{account: user, transactions: location.state.transactions}}>Create a transaction</Link>
                 </TransactionButton>
                 <TransactionButton>
                     <Link to="/home">Return Home</Link>
