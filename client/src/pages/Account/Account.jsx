cde9bb84109df4f78d5f743a0719e25f8248bd3c
JvsonKee <jasonk2723@gmail.com>
2024-04-13 06:15:40 -0600

    removed incomes from account page

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -2,13 +2,11 @@ import NavBar from '../../components/NavBar';
 import { PageContainer, MainContainer } from '../../styles/Containers';
 import { ProfileContentContainer, ProfileContainer, AccountContentContainer, Left, Right, AccountItem, AccountNav, LogoutLink, MainContent, Icon } from './Account.styled';
 import AccountInformation from './AccountInformation';
-import { faArrowRightFromBracket, faAddressCard, faCreditCard, faCircleDollarToSlot } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faAddressCard, faCreditCard } from '@fortawesome/free-solid-svg-icons';
 import PaymentMethods from './PaymentMethods';
 import { useState } from 'react';
 
 const Account = () => {
-
-    // const [user, setUser] = useContext(UserContext)
     const [active, setActive] = useState("account")
 
     const handleClick = (page) => {
@@ -24,7 +22,6 @@ const Account = () => {
                         <AccountContentContainer>
                             <Left>  
                                 <AccountNav>
-                                    <AccountItem onClick={() => handleClick("income")}><Icon icon={faCircleDollarToSlot}/><span>Income</span></AccountItem>
                                     <AccountItem onClick={() => handleClick("payment")}><Icon icon={faCreditCard}/><span>Payment Methods</span></AccountItem>
                                     <AccountItem onClick={() => handleClick("account")}><Icon icon={faAddressCard}/><span>Account Information</span></AccountItem>
                                     <AccountItem><LogoutLink to="/"><Icon icon={faArrowRightFromBracket}/><span>Logout</span></LogoutLink></AccountItem>
