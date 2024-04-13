a2417592cf46565b4db0f2eca01008a84bc76d1c
JvsonKee <jasonk2723@gmail.com>
2024-04-13 06:16:30 -0600

    Goals for homepage box

---
+++ client/src/pages/Home/GoalsHome.jsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../../App"
+import { BoxContentWrapper, BoxHeader, ColumnBoxContainer, IncomeAmount, IncomeItem, IncomeName } from "./Home.styled"
+import axios from "axios"
+
+const GoalsHome = () => {
+    const [user, setUser] = useContext(UserContext)
+    const [goals, setGoals] = useState([])
+
+    useEffect(() => {
+        const fetchAllGoals = async () => {
+            try{
+                const res = await axios.get("http://localhost:8800/goals/" + user.userID)
+                setGoals(res.data)
+                console.log(res)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllGoals()
+    },[user.userID])
+
+    return (
+        <ColumnBoxContainer>
+            <BoxHeader>Goals</BoxHeader>
+            <BoxContentWrapper>
+                {
+                    goals.map((goal, key) => (
+                        <IncomeItem key={key}>
+                            <IncomeName>{goal.title}</IncomeName>
+                            <IncomeAmount style={{color: '#FFA723'}}>${goal.amount}</IncomeAmount>
+                        </IncomeItem>
+                    ))
+                }
+            </BoxContentWrapper>
+        </ColumnBoxContainer>
+    )
+}
+
+export default GoalsHome;
